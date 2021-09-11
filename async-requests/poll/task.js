639a20d4fc9df751135f144909b532b2a2d9349c
Pavel Titov <otrepiev.grishka@yandex.ru>
2021-09-11 03:36:44 +0300

    async-request

--- async-requests/poll/task.js
+++ async-requests/poll/task.js
@@ -1,4 +1,8 @@
 const url = "https://netology-slow-rest.herokuapp.com/poll.php";
+let question = document.getElementById("poll__title");
+let answer = document.getElementById("poll__answers");
+const modal = document.getElementById("modal");
+const btnClose = document.getElementById("close-button");
 
 let xhr = new XMLHttpRequest();
 xhr.open("GET", url, true);
@@ -6,6 +10,21 @@ xhr.send();
 
 xhr.onload = () => {
     if (xhr.status === 200) {
-       console.log(JSON.parse(xhr.response));
+        let response = JSON.parse(xhr.response);
+        let answers = response.data.answers;
+        question.textContent = response.data.title;
+        for (let item in answers) {
+            let btn = document.createElement("button");
+            btn.setAttribute("style", "margin: 6px;");
+            btn.textContent = answers[item];
+            answer.appendChild(btn);
+            btn.onclick = () => {
+                modal.classList.add("modal_active");
+            }
+        }
     }
+}
+
+btnClose.onclick = () => {
+    modal.classList.remove("modal_active");
 }
